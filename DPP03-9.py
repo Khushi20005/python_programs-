40112e78221e5490288963e1accb58869e2b394f
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:30:19 +0530

    Add files via upload

---
+++ DPP03-9.py
@@ -0,0 +1,9 @@
+#Tuple Unpacking: Given the tuple coords = (3, 5, 7), unpack the tuple into three
+#variables and print each one
+coords = (6, 9, 2)
+x = coords[0]
+y = coords[1]
+z = coords[2]
+print("x:", x)
+print("y:", y)
+print("z:", z)
