0ad6a622d2a38d91d80f372894fcc2affd75be46
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:26:47 +0530

    Add files via upload

---
+++ DPP03-3.py
@@ -0,0 +1,8 @@
+#Slicing: For the list fruits = ['apple', 'banana', 'cherry', 'date',
+#'elderberry'], slice the list to print the first three elements.
+"""fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+first_three_fruits = fruits[:3]
+print(first_three_fruits)"""
+
+fruits=['apple', 'banana', 'cherry', 'date', 'elderberry']
+print(fruits[0:3])
