d1413ee1b3cf1374933d17063e48f3b64ed69526
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:27:30 +0530

    Add files via upload

---
+++ DPP03-4.py
@@ -0,0 +1,7 @@
+#List Methods: Given the list colors = ['red', 'green', 'blue'], add 'yellow'
+#at the end of the list and then remove 'green'
+colors=['red', 'green', 'blue']
+colors.append('yellow')
+print(colors)
+colors.remove('green')
+print(colors)
