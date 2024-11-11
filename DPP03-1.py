50e49739b5b5879b8ca5849b7de74fbaf12cdb0c
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 12:04:59 +0530

    Add files via upload

---
+++ DPP03-1.py
@@ -0,0 +1,6 @@
+#Basic List Creation: Create a list of 5 integers and print the sum of all the elements.
+list1=[1,2,3,4,5]
+sum=0
+for i in list1:
+    sum+=i
+print(" the sum of all elements",sum)
