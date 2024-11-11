75c9eaab78b2b96054cce60df409d80dcf2dffbd
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:30:43 +0530

    Add files via upload

---
+++ DPP03-10.py
@@ -0,0 +1,6 @@
+#Tuple Concatenation: Concatenate two tuples (1, 2, 3) and (4, 5, 6) and print
+#the result.
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+result = tuple1 + tuple2
+print("Concatenated Tuple:", result)
