21919e472cb80abc8d4a3cb12097710aa3dc7d9f
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:28:38 +0530

    Add files via upload

---
+++ DPP03-5.py
@@ -0,0 +1,4 @@
+#List Comprehension: Generate a list of squares of numbers from 1 to 10 using list
+#comprehension.
+list_of_square = [x**2 for x in range(1,11)]
+print("list of squares of numbers from 1 to 10",list_of_square)
