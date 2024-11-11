180817132ee98a3e4e7476a46a159e294f8afcbf
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:31:48 +0530

    Add files via upload

---
+++ DPP03-13.py
@@ -0,0 +1,9 @@
+#Basic Dictionary Creation: Create a dictionary where the keys are fruits and the values
+#are their prices. Print the price of an 'apple'.
+fruit_prices = {
+    'apple': 100,
+    'banana': 60,
+    'orange': 75,
+    'grape': 30
+}
+print("The price of an apple is:", fruit_prices['apple'])
