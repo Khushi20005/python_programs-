ff011f2c18f5a701adec032443e95af69b68e2d6
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 13:32:08 +0530

    Add files via upload

---
+++ DPP03-14.py
@@ -0,0 +1,9 @@
+#Adding and Modifying Entries: Add a new entry 'orange': 30 to the dictionary
+#{'apple': 50, 'banana': 20} and modify the price of 'banana' to 25.
+fruit_prices = {
+    'apple': 50,
+    'banana': 20
+}
+fruit_prices['orange'] = 30
+fruit_prices['banana'] = 25
+print("Updated dictionary:", fruit_prices)
