c72fb5e04b3dfff1ab219a586ad9e2ba197770be
Khushi20005 <khushisinghh205@gmail.com>
2024-11-11 12:01:16 +0530

    Add files via upload

--- DPP02-10.py
+++ DPP02-10.py
@@ -1,3 +1,8 @@
 """ Write a python program that takes a string as input and returns a new string where each words first letter is capitalize 
 (also known as "title case")for example, the input "hello world" should be return "Hello World". considered edge cases like multiple spaces between
 words."""
+def title_case(sentence):
+    words = sentence.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
+print(title_case("hello world")) 
