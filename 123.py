7f12ba3573c2ac773a143ea5b3c41fb90a151eaf
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:03:48 +0530

    Delete 123.py

--- 123.py
+++
@@ -1,6 +0,0 @@
-#write a program to check wheather a person is eligible for voting or not (age>=18).
-a=int(input("enter your age"))
-if(a>=18):
-    print("you are eligible for vote")
-else:
-    print("you are not eligible for vote")
