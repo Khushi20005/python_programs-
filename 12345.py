d92a83a25bf3a36bd5a36353fb8027f0073689ce
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:04:57 +0530

    Delete 12345.py

--- 12345.py
+++
@@ -1,8 +0,0 @@
-# write a program to check wheather a number entered by user divisible is by 2 or 3 (entered number should not be greater than 100).
-a=int(input("enter any number"))
-if(a>=100):
-    print("the entered number should not be greater than 100")
-elif(a%2==0 and a%3==0):
-    print("the number is divisible by 2 and 3 both")
-else:
-    print("the number is not divisible by 2 and 3 both")
