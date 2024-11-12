7f3e0130124d5e4181391cca656d2fa031a4cbdd
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:05:40 +0530

    Delete 12345678.py

--- 12345678.py
+++
@@ -1,9 +0,0 @@
-#Write a program to check wheather a number is prime or not.
-a=int(input("enter the value of a:"))
-for i in range(2, a-1):
-    if a%i !=0:
-        print(a,"number is prime number")
-        break
-    else:
-        print(a,"the number is not a prime number")
-    break
