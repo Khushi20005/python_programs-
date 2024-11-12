b165ca93a569a185a9c8f427a83becb70b8a0992
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:10:32 +0530

    Delete count_digit.py

--- count_digit.py
+++
@@ -1,7 +0,0 @@
-#Write a python program to count total number of digits in a number.
-number=(input("enter the number:"))
-digit_count=0
-for char in number:
-    if char.isdigit():
-        digit_count +=1
-print("total number of digits:",digit_count)
