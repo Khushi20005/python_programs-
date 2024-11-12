6239250088c2ee5499d94789a610db12516e9e9d
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:13:50 +0530

    Delete leap_year_not.py

--- leap_year_not.py
+++
@@ -1,7 +0,0 @@
-#WAP to check an given year is leap year or not.
-year=int(input("enter the year"))
-a=year%100
-if(a%4==0 and a%100!=0 or a%400==0):
-    print("this is leap year")
-else:
-    print("this is not a leap year")
