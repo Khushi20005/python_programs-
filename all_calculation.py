2869c037843484a32e98f332a4329d2c35e801f6
Khushi20005 <khushisinghh205@gmail.com>
2024-11-12 11:08:11 +0530

    Delete all_calculation.py

--- all_calculation.py
+++
@@ -1,8 +0,0 @@
-#WAP that take 2 number that perform floar,division,subtract,multiplication and addition.
-a=int(input("enter the number a:"))
-b=int(input("enter the number b:"))
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a//b)
